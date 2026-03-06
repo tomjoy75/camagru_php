6a01557d10472a788f268b33fc44cb8c24d74697
tomjoy75 <tomjoy75@gmail.com>
2026-03-06 01:03:21 +0100

    Add registration form view

---
+++ src/views/register.php
@@ -0,0 +1,19 @@
+<form method="post" action="/register">
+    <div>
+        <label for="email">Email</label>
+        <input type="email" id="email" name="email" required>
+    </div>
+    <div>
+        <label for="username">Username</label>
+        <input type="text" id="username" name="username" required>
+    </div>
+    <div>
+        <label for="password">Password</label>
+        <input type="password" id="password" name="password" required>
+    </div>
+    <div>
+        <label for="confirm_password">Confirm password</label>
+        <input type="password" id="confirm_password" name="confirm_password" required>
+    </div>
+    <button type="submit">Register</button>
+</form>
