5f0551cae7f7a44e45b3f7c0a1922c3db179f65d
tomjoy75 <tomjoy75@gmail.com>
2026-03-09 15:47:25 +0100

    Add the EditorController with a show() method

---
+++ src/controller/EditorController.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Handles editor page. Requires user to be logged in.
+ */
+class EditorController
+{
+    public static function show(): void
+    {
+        if (!isset($_SESSION['user_id']) || $_SESSION['user_id'] === '') {
+            header('Location: /login');
+            exit;
+        }
+
+        header('Content-Type: text/html; charset=utf-8');
+        $view = 'editor.php';
+        require __DIR__ . '/../views/layout.php';
+    }
+}
